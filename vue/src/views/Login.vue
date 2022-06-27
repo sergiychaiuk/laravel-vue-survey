ae08c1718874367e58e03508941eb7b8e574bb33
serhiichaiuk <chayck7@gmail.com>
2022-06-27 14:15:56 +0300

    added axios, login on frontend

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -19,13 +19,39 @@
       </router-link>
     </p>
   </div>
-  <form class="mt-8 space-y-6" action="#" method="POST">
+  <form class="mt-8 space-y-6" @submit="login">
+    <div
+      v-if="errorMsg"
+      class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded"
+    >
+      {{ errorMsg }}
+      <span
+        class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]"
+        @click="errorMsg = ''"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </span>
+    </div>
     <input type="hidden" name="remember" value="true" />
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
         <label for="email-address" class="sr-only">Email address</label>
         <input
           id="email-address"
+          v-model="user.email"
           name="email"
           type="email"
           autocomplete="email"
@@ -38,6 +64,7 @@
         <label for="password" class="sr-only">Password</label>
         <input
           id="password"
+          v-model="user.password"
           name="password"
           type="password"
           autocomplete="current-password"
@@ -52,6 +79,7 @@
       <div class="flex items-center">
         <input
           id="remember-me"
+          v-model="user.remember"
           name="remember-me"
           type="checkbox"
           class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
@@ -81,4 +109,31 @@
 
 <script setup>
 import { LockClosedIcon } from "@heroicons/vue/solid";
+import store from "../store";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+
+const user = {
+  email: "",
+  password: "",
+  remember: false,
+};
+let errorMsg = ref("");
+
+function login(ev) {
+  ev.preventDefault();
+
+  store
+    .dispatch("login", user)
+    .then(() => {
+      router.push({
+        name: "Dashboard",
+      });
+    })
+    .catch((err) => {
+      errorMsg.value = err.response.data.message;
+    });
+}
 </script>
