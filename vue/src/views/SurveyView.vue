d5cde3059b793430397a51bce0eddaeb7d4666ee
serhiichaiuk <chayck7@gmail.com>
2022-06-30 15:29:33 +0300

    created survey form

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -1,9 +1,163 @@
 <template>
-  <PageComponent title="View or create a survey">
-    View or create a survey
+  <PageComponent>
+    <template #header>
+      <div class="flex items-center justify-between">
+        <h1 class="text-3xl font-bold text-gray-900">
+          {{ model.id ? model.title : "Create a Survey" }}
+        </h1>
+      </div>
+    </template>
+    <form @submit.prevent="saveSurvey">
+      <div class="shadow sm:rounded-md sm:overflow-hidden">
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+          <!-- Image -->
+          <div class="text-left">
+            <label> Image </label>
+            <div class="mt-1 flex items-center">
+              <img
+                v-if="model.image"
+                :src="model.image"
+                :alt="model.title"
+                class="w-64 h-48 object-cover"
+              />
+              <span
+                v-else
+                class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-[80%] w-[80%] text-gray-300"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+              <button
+                type="button"
+                class="relative overflow-hidden ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <input
+                  type="file"
+                  class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer"
+                />
+                Change
+              </button>
+            </div>
+          </div>
+          <!--/ Image -->
+
+          <!-- Title -->
+          <div class="text-left">
+            <label for="title" class="block text-sm font-medium text-gray-700">
+              Title
+            </label>
+            <input
+              id="title"
+              v-model="model.title"
+              type="text"
+              name="title"
+              autocomplete="survey_title"
+              class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+            />
+          </div>
+          <!--/ Title -->
+
+          <!-- Description -->
+          <div class="text-left">
+            <label
+              for="description"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Description
+            </label>
+            <textarea
+              id="description"
+              v-model="model.description"
+              name="description"
+              rows="3"
+              autocomplete="survey_description"
+              class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+              placeholder="Describe your survey"
+            />
+          </div>
+          <!--/ Description -->
+
+          <!-- Expire Date -->
+          <div class="text-left">
+            <label
+              for="expire_date"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Expire Date
+            </label>
+            <input
+              id="expire_date"
+              v-model="model.expire_date"
+              type="date"
+              name="expire_date"
+              class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+            />
+          </div>
+          <!--/ Expire Date -->
+
+          <!-- Status -->
+          <div class="flex items-start">
+            <div class="flex items-center h-5">
+              <input
+                id="status"
+                v-model="model.status"
+                name="status"
+                type="checkbox"
+                class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+              />
+            </div>
+            <div class="ml-3 text-sm">
+              <label for="status" class="font-medium text-gray-700">
+                Active
+              </label>
+            </div>
+          </div>
+          <!--/ Status -->
+        </div>
+        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+          <button
+            type="submit"
+            class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
   </PageComponent>
 </template>
 
 <script setup>
+import store from "../store";
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+
 import PageComponent from "../components/PageComponent.vue";
+
+const route = useRoute();
+
+let model = ref({
+  title: "",
+  status: false,
+  description: null,
+  image: null,
+  expire_date: null,
+  question: [],
+});
+
+if (route.params.id) {
+  model.value = store.state.surveys.find(
+    (s) => s.id === parseInt(route.params.id)
+  );
+}
 </script>
