52d458750971d0a0502f1600bd296773c34ca582
serhiichaiuk <chayck7@gmail.com>
2022-07-03 16:51:19 +0300

    show notification on survey update

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -126,6 +126,8 @@
     </Disclosure>
 
     <router-view></router-view>
+
+    <Notification />
   </div>
 </template>
 
@@ -143,6 +145,7 @@ import { BellIcon, MenuIcon, XIcon } from "@heroicons/vue/outline";
 import { useStore } from "vuex";
 import { computed } from "vue";
 import { useRouter } from "vue-router";
+import Notification from "./Notification.vue";
 
 const navigation = [
   { name: "Dashboard", to: { name: "Dashboard" } },
@@ -151,6 +154,7 @@ const navigation = [
 
 export default {
   components: {
+    Notification,
     Disclosure,
     DisclosureButton,
     DisclosurePanel,
