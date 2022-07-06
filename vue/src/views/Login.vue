dd85713033e4834fafd282b1221ea87d65f3118f
serhiichaiuk <chayck7@gmail.com>
2022-07-06 19:59:56 +0300

    show validation errors

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -20,10 +20,7 @@
     </p>
   </div>
   <form class="mt-8 space-y-6" @submit="login">
-    <div
-      v-if="errorMsg"
-      class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded"
-    >
+    <Alert v-if="errorMsg">
       {{ errorMsg }}
       <span
         class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]"
@@ -44,7 +41,7 @@
           />
         </svg>
       </span>
-    </div>
+    </Alert>
     <input type="hidden" name="remember" value="true" />
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
@@ -92,8 +89,13 @@
 
     <div>
       <button
+        :disabled="loading"
         type="submit"
         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        :class="{
+          'cursor-not-allowed': loading,
+          'hover:bg-indigo-500': loading,
+        }"
       >
         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
           <LockClosedIcon
@@ -101,6 +103,28 @@
             aria-hidden="true"
           />
         </span>
+
+        <svg
+          v-if="loading"
+          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
         Sign in
       </button>
     </div>
@@ -112,6 +136,7 @@ import { LockClosedIcon } from "@heroicons/vue/solid";
 import store from "../store";
 import { useRouter } from "vue-router";
 import { ref } from "vue";
+import Alert from "../components/Alert.vue";
 
 const router = useRouter();
 
@@ -120,20 +145,25 @@ const user = {
   password: "",
   remember: false,
 };
+let loading = ref(false);
 let errorMsg = ref("");
 
 function login(ev) {
   ev.preventDefault();
 
+  loading.value = true;
+
   store
     .dispatch("login", user)
     .then(() => {
+      loading.value = false;
       router.push({
         name: "Dashboard",
       });
     })
     .catch((err) => {
-      errorMsg.value = err.response.data.message;
+      loading.value = false;
+      errorMsg.value = err.response.data.error;
     });
 }
 </script>
