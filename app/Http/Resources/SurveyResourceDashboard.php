1376198563db35abddf767b8af088ddd773151d9
serhiichaiuk <chayck7@gmail.com>
2022-07-06 19:11:21 +0300

    implemented dashboard

--- app/Http/Resources/SurveyResourceDashboard.php
+++ app/Http/Resources/SurveyResourceDashboard.php
@@ -4,8 +4,9 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\URL;
+use Nette\Utils\DateTime;
 
-class SurveyResource extends JsonResource
+class SurveyResourceDashboard extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -20,12 +21,11 @@ class SurveyResource extends JsonResource
             'image_url' => $this->image ? URL::to($this->image) : null,
             'title' => $this->title,
             'slug' => $this->slug,
-            'status' => $this->status !== 'draft',
-            'description' => $this->description,
-            'created_at' => (new \DateTime($this->created_at))->format('Y-m-d H:i:s'),
-            'updated_at' => (new \DateTime($this->updated_at))->format('Y-m-d H:i:s'),
-            'expire_date' => (new \DateTime($this->expire_date))->format('Y-m-d'),
-            'questions' => SurveyQuestionResource::collection($this->questions)
+            'status' => !!$this->status,
+            'created_at' => (new DateTime($this->created_at))->format('Y-m-d H:i:s'),
+            'expire_date' => $this->expire_date,
+            'questions' => $this->questions()->count(),
+            'answers' => $this->answers()->count()
         ];
     }
 }
